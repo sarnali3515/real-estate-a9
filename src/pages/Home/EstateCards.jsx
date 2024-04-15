4476672a97ff9ee7888841b6501aabfe36654c44
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-04-15 23:21:56 +0600

    estate card updated with details

--- src/pages/Home/EstateCards.jsx
+++ src/pages/Home/EstateCards.jsx
@@ -1,28 +1,64 @@
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { FaChartArea } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoMdCheckmark } from "react-icons/io";
+
 
 
 const EstateCards = ({ estate }) => {
 
-    const { estate_title, image_url, segment_name, status } = estate;
+    const { id, estate_title, image_url, segment_name, status, description, area, location, facilities } = estate;
 
     return (
-        <div className="card bg-base-100 shadow-xl">
+        <div className="card bg-indigo-50 shadow-xl">
             <figure ><img src={image_url} alt="Shoes" /></figure>
+            <div className="flex justify-between">
+                <div className="badge text-xl p-3 text-white bg-indigo-800">{segment_name}</div>
+                <div>
+                    {
+                        status == 'sale' ?
+                            <div className="badge text-xl p-3 text-white bg-red-700">Sale</div>
+                            :
+                            <div className="badge text-xl p-3 text-white bg-lime-600">Rent</div>
+                    }
+                </div>
+            </div>
             <div className="card-body">
                 <h2 className="card-title">
                     {estate_title}
-                    <div className="badge text-white bg-indigo-800">{segment_name}</div>
+
                 </h2>
-                {
-                    status == 'sale' ?
-                        <div></div>
+
+                <p>{
+                    description.length > 80 ?
+                        <p>{description.slice(0, 80)} <Link
+                            className="text-blue-600 font-bold"
+                            to={`/news/${id}`}
+                        >Read More...</Link></p>
                         :
-                        <div>xyz</div>
+                        <p>{description}</p>
                 }
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
+                </p>
+                <p className="flex gap-2 items-center"><FaChartArea></FaChartArea>Area: <span className="font-semibold">{area}</span></p>
+                <hr />
+                <p className="flex gap-2 items-center"><FaLocationDot></FaLocationDot>Location: <span className="font-semibold">{location}</span></p>
+                <hr />
+                <ul className="text-left ml-5">
+                    <span className="font-bold text-lg">Facilities</span>
+                    {
+                        facilities.map((facility, idx) =>
+                            <li
+                                key={idx}
+                                className="flex items-center gap-3"
+                            ><IoMdCheckmark></IoMdCheckmark><a>{facility}</a>
+                            </li>)
+                    }
+                </ul>
+                {/* <div className="card-actions justify-end">
                     <div className="badge badge-outline">Fashion</div>
                     <div className="badge badge-outline">Products</div>
-                </div>
+                </div> */}
                 <button className="btn text-white bg-lime-600">View Property</button>
             </div>
         </div>
